5c3d78ad086aa5f90a9241dcf9b5cb7326c4a639
Njáll Hilmar Hilmarsson <njall13@ru.is>
2017-03-22 16:14:09 +0000

    working on parser

--- java/Parser.java
+++ java/Parser.java
@@ -2,6 +2,7 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
 import java.io.*;
 
 class Parser {
@@ -18,15 +19,33 @@ class Parser {
 
 		// While loop variables
 		int length = 0;
-		String line;
-
+		String line = null, nodeName = null;
+		List currNode = null;
 		while((line = buffer.readLine()) != null) {
-			if( line.contains("CallGraph Root is:") )
+			if( line.length() == 0 ) {
+				if( currNode == null || nodeName == null ) { 
+					System.out.println("Error occured during parsing");
+					return;
+				}
+				callgraph.put(nodeName, currNode);
+			} else if( line.contains("CallGraph Root is:") )
 				root = line.substring(19);
+			else if(line.contains("Call graph node for function:")) {
+				currNode = new ArrayList<String>();
+				line = line.substring(31);
+				nodeName = line.substring(0, line.indexOf('\''));
+			} else if(line.contains(" calls function ")) {
+				if(currNode != null) {
+					line = line.substring(26, line.length()-1);
+					System.out.println("function call: " + line);
+					currNode.add(line);
+				} else
+					continue;
+			}
 
 			length = length + line.length();
 
-			System.out.println(line);
+			System.out.println(line.length() + ": " + line);
 		}
 
 		System.out.println("root: " + root);
