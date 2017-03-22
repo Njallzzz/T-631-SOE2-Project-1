873fc8ad17fc2733422ba58a5fc4c2a3ac4a4fc2
Njáll Hilmar Hilmarsson <njall13@ru.is>
2017-03-22 16:50:24 +0000

    finished parser minus fixing warnings

--- java/Parser.java
+++ java/Parser.java
@@ -19,13 +19,11 @@ class Parser {
 		BufferedReader buffer = new BufferedReader(instream);
 
 		// While loop variables
-		int length = 0;
 		String line = null, nodeName = null;
 		List currNode = null;
-		while((line = buffer.readLine()) != null) {
-			length = length + line.length();
-			System.out.println(line.length() + ": " + line);
 
+		// Parser main while loop
+		while((line = buffer.readLine()) != null) {
 			if( line.length() == 0 ) {
 				if( currNode != null || nodeName != null ) { 
 					callgraph.put(nodeName, currNode); 
@@ -34,28 +32,23 @@ class Parser {
 				root = line.substring(19);
 			} else if(line.contains("Call graph node for function:")) {
 				currNode = new ArrayList<String>();
-				line = line.substring(31);
-				nodeName = line.substring(0, line.indexOf('\''));
+				nodeName = line.substring(31, line.indexOf('\''));
 			} else if(line.contains(" calls function ")) {
 				if(currNode != null) {
 					line = line.substring(line.indexOf('\'')+1, line.length()-1);
-					System.out.println("function call: " + line);
 					currNode.add(line);
-				} else
-					continue;
+				}
 			}
-
 		}
 
+		// Debugging viewer of function
 		for (Map.Entry<String, List<String>> entry : callgraph.entrySet()) {
 			String key = entry.getKey();
 			List<String> value = entry.getValue();
-			System.out.println(key);
-
+			System.out.println("\nKey: " + key);
 			for(int i = 0; i < value.size(); i++) {
-				System.out.println("entry: " + value.get(i));
+				System.out.println("Entry: " + value.get(i));
 			}
 		}
-
 	}
 }
