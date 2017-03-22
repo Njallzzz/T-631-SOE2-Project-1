ddc9442aa78c956c3b1b72cde156785700804dcb
Njáll Hilmar Hilmarsson <njall13@ru.is>
2017-03-22 15:26:52 +0000

    basic parser stuff

--- java/Parser.java
+++ java/Parser.java
@@ -1,10 +1,23 @@
 // To generate callgraph: opt -print-callgraph <.bc file here> 2>&1 1> /dev/null
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.io.*;
 
 class Parser {
+	private static Map<String, List<String>> callgraph;
+
 	public static void main (String [] args) throws java.io.IOException {
-		int ch;
-		System.out.print ("Enter some text: ");
-		while ((ch = System.in.read ()) != '\n')
-			System.out.print ((char) ch);
+		callgraph = new HashMap<String, List<String>>();
+
+		InputStreamReader instream = new InputStreamReader(System.in);
+		BufferedReader buffer = new BufferedReader(instream);
+	
+		line = buffer.readLine();
+
+		while (line!=null){
+			length = length + line.length();
+			line= buffer.readLine();
+		}
 	}
 }
