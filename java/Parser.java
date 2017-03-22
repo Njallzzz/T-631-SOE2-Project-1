fa94309dee5993fbb5aaa2e6cd155240f046ae94
Njáll Hilmar Hilmarsson <njall13@ru.is>
2017-03-22 16:34:09 +0000

    Testing parser

--- java/Parser.java
+++ java/Parser.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.*;
 
 class Parser {
@@ -22,33 +23,51 @@ class Parser {
 		String line = null, nodeName = null;
 		List currNode = null;
 		while((line = buffer.readLine()) != null) {
+			length = length + line.length();
+			System.out.println(line.length() + ": " + line);
+
 			if( line.length() == 0 ) {
-				if( currNode == null || nodeName == null ) { 
-					System.out.println("Error occured during parsing");
-					return;
+				if( currNode != null || nodeName != null ) { 
+					callgraph.put(nodeName, currNode); 
 				}
-				callgraph.put(nodeName, currNode);
-			} else if( line.contains("CallGraph Root is:") )
+			} else if( line.contains("CallGraph Root is:") ) {
 				root = line.substring(19);
-			else if(line.contains("Call graph node for function:")) {
+			} else if(line.contains("Call graph node for function:")) {
 				currNode = new ArrayList<String>();
 				line = line.substring(31);
 				nodeName = line.substring(0, line.indexOf('\''));
 			} else if(line.contains(" calls function ")) {
 				if(currNode != null) {
-					line = line.substring(26, line.length()-1);
+					line = line.substring(line.indexOf('\'')+1, line.length()-1);
 					System.out.println("function call: " + line);
 					currNode.add(line);
 				} else
 					continue;
 			}
 
-			length = length + line.length();
-
-			System.out.println(line.length() + ": " + line);
 		}
 
-		System.out.println("root: " + root);
-		System.out.println("Length: " + length);
+		for (Map.Entry<String, List<String>> entry : callgraph.entrySet()) {
+			String key = entry.getKey();
+			List<String> value = entry.getValue();
+			System.out.println(key);
+
+			for(int i = 0; i < value.size(); i++) {
+				System.out.println("entry: " + value.get(i));
+			}
+		}
+
+		/*Iterator it = callgraph.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry)it.next();
+			System.out.println(pair.getKey() + " = ");
+			ArrayList lis = pair.getValue();
+			for(int i = 0; i < lis.size(); i++) {
+				System.out.println("entry: " + lis.get(i));
+			}
+
+			it.remove(); // avoids a ConcurrentModificationException
+		}	// */
+
 	}
 }
